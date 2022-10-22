c7b9caade0a8b2a89c169987f9e446e0c9f3e817
Neleon10 <cvn45@hotmail.com>
2022-10-22 01:20:45 -0300

    adding filter

--- src/Componets/tableTest/TableTest.jsx
+++ src/Componets/tableTest/TableTest.jsx
@@ -8,6 +8,9 @@ import TableTestModal from "./TableTestModal";
 
 
 function TableResponsive() {
+
+    //filter 
+    const [filter, setFilter] = React.useState('');
     
     //cases from Redux (store)
     const cases = useSelector((state) => state.cases)
@@ -23,10 +26,30 @@ function TableResponsive() {
         setShowModal(true)
     }
 
-   
+
+    //Filter method
+    let casesFiltered = [];
+    if(!filter){
+        casesFiltered = cases;
+    }else{
+        casesFiltered =  cases.filter((dato)=>
+             dato.Nombre.toLowerCase().includes(filter.toLocaleLowerCase()) || dato.Numero.toLowerCase().includes(filter.toLocaleLowerCase()) || dato.perito.toLowerCase().includes(filter.toLocaleLowerCase()) || dato.localidad.toLowerCase().includes(filter.toLocaleLowerCase()) || dato.Compañia.toLowerCase().includes(filter.toLocaleLowerCase())
+        )
+    }
+
+  
     return (
         <>
-            {cases?.length === 0 ?
+         <input id="filter"
+          name="filter"
+          type="text"
+          value={filter} //--> binding input with state.
+          onChange={e => setFilter(e.target.value)}
+        />
+
+
+
+            {casesFiltered?.length === 0 ?
                 <div>Loading...</div> :
                 <Table>
                     <Thead>
@@ -41,31 +64,31 @@ function TableResponsive() {
                             <Th className="thEdit">Localidad</Th>
                             <Th className="thEdit">N° Contacto</Th>
                             <Th className="thEdit">Estado</Th>
-                            <Th className="thEdit">Perito</Th>
+                            <Th className="thEdit">Perito</Th>  
                             <Th className="thEdit">Notas Extras</Th>
                             <Th className="thEdit">Editar</Th>
                         </Tr>
                     </Thead>
                     <Tbody>
                         {
-                            cases.map((el) => (
+                         casesFiltered.map((el) => (  
                                 // Patente is a notnull field meaning that an empty register won't be allowed.
 
                                 el.Patente &&
                                 <Tr>
-                                    <Td className="tdEdit">{el.Vencimiento}</Td>
-                                    <Td className="tdEdit">{el.Numero}</Td>
-                                    <Td className="tdEdit">{el.Compañia}</Td>
-                                    <Td className="tdEdit">{el.Nombre}</Td>
-                                    <Td className="tdEdit">{el.Patente}</Td>
-                                    <Td className="tdEdit">{el.Marca}</Td>
-                                    <Td className="tdEdit">{el.direccion}</Td>
-                                    <Td className="tdEdit">{el.localidad}</Td>
-                                    <Td className="tdEdit">{el.celular}</Td>
-                                    <Td className="tdEdit">{!el.estado ? "Sin Completar" : el.estado}</Td>
-                                    <Td className="tdEdit">{el.perito}</Td>
-                                    <Td className="tdEdit">{!el.notas ? "Sin Completar" : el.notas}</Td>
-                                    <Td className="tdEdit"><div className="editBtn" onClick={() => showModalEdit(el.id)}><TbEdit /></div></Td>
+                                    <Td className="tdEdit" key={el.id}>{el.Vencimiento}</Td>
+                                    <Td className="tdEdit" key={el.id}>{el.Numero}</Td> 
+                                    <Td className="tdEdit" key={el.id} >{el.Compañia}</Td>
+                                    <Td className="tdEdit" key={el.id}>{el.Nombre}</Td>
+                                    <Td className="tdEdit" key={el.id}>{el.Patente}</Td>
+                                    <Td className="tdEdit" key={el.id}>{el.Marca}</Td>
+                                    <Td className="tdEdit" key={el.id}>{el.direccion}</Td>
+                                    <Td className="tdEdit" key={el.id}>{el.localidad}</Td>
+                                    <Td className="tdEdit" key={el.id}>{el.celular}</Td>
+                                    <Td className="tdEdit" key={el.id}>{!el.estado ? "Sin Completar" : el.estado}</Td>
+                                    <Td className="tdEdit" key={el.id}>{el.perito}</Td>
+                                    <Td className="tdEdit" key={el.id}>{!el.notas ? "Sin Completar" : el.notas}</Td>
+                                    <Td className="tdEdit" key={el.id}><div className="editBtn" onClick={() => showModalEdit(el.id)}><TbEdit /></div></Td>
                                 </Tr>
                             ))
                         }
