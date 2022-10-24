d5955dd1ad1b3ce7d10043eacfe802e3da69ed52
Carlos Vazquez Nosetto <78918123+neleon10@users.noreply.github.com>
2022-10-23 23:17:24 -0300

    Update NavBarTest.jsx

--- src/Componets/NavBar/NavBarTest.jsx
+++ src/Componets/NavBar/NavBarTest.jsx
@@ -21,8 +21,8 @@ function NavScrollExample() {
                 <Navbar.Collapse  id="navbarScroll">
                     <Nav
                         className="me-auto my-2 my-lg-0"
-                        style={{ maxHeight: '100px' }}
-                        navbarScroll
+                        style={{ maxHeight: '200px' }}
+                        //navbarScroll
                     >
                         <div className="listNavBar">
                             <div className='navBarLinks'>
