e25c3931c7f4c6d495fd11cba47417f74ad149a5
4Bprogramming <114164723+4Bprogramming@users.noreply.github.com>
2022-10-28 17:42:28 -0300

    login

--- src/Componets/Login/Login.jsx
+++ src/Componets/Login/Login.jsx
@@ -1,18 +1,12 @@
 import React, { useState, useEffect } from "react";
 import firebaseApp from "../../Credentials/Index";
-
-import profile from "../../assets/a.png";
-
 import { Button } from "react-bootstrap";
 import {
   getAuth,
   signInWithEmailAndPassword,
   GoogleAuthProvider,
-  signInWithRedirect,
   signInWithPopup,
 } from "firebase/auth";
-import { auth, provider } from "../../Credentials/index";
-import * as C from "./styles";
 import { getFirestore, doc, setDoc } from "firebase/firestore";
 import { useNavigate, Link } from "react-router-dom";
 import { getCasesAction, getPeritos, peritosByName } from "../../Store/Actions";
@@ -22,7 +16,7 @@ const auth = getAuth(firebaseApp);
 const googleProvider = new GoogleAuthProvider();
 
 function Login() {
-  const [emailUser, setEmailUser] = useLocalStorage("emailUser", "");
+  const [emailUser, setEmailUser]= useLocalStorage('emailUser','')
   let dispatch = useDispatch();
   useEffect(() => {
     dispatch(getPeritos());
@@ -38,81 +32,81 @@ function Login() {
   const navigate = useNavigate();
   const peritos = useSelector((state) => state.peritos);
 
-  /*  async function loginEmailPassword(email, password) {
+  async function loginEmailPassword(email, password) {
     signInWithEmailAndPassword(auth, email, password);
-  } */
-
-  // async function submitHandler(e) {
-  //   e.preventDefault();
-  //   let booll = peritos.some((el) => el.email === "email");
-  //   if (booll) {
-  //     const email = e.target.elements.email.value;
-  //     const password = e.target.elements.password.value;
-  //     // const correo = document.getElementById("formCorreo").value;
-  //     // const contra = e.target.formContra.value;
-  //     await loginEmailPassword(email, password);
-  //   }
-  // }
-
-//  const submitHandlerGoogle = (e) => {
-//     /* console.log('e.target', e.target) */
-//     let booll = peritos.some((el) => el.email === "email");
-
-//     try {
-//       signInWithPopup(auth, googleProvider).then((result) => {
-//         // This gives you a Google Access Token. You can use it to access the Google API.
-//         // const credential = GoogleAuthProvider.credentialFromResult(result);
-//         // console.log(credential)
-//         // const token = credential.accessToken;
-//         // The signed-in user info.
-//         const user = result.user;
-//         console.log("user", user);
-//         let userPerito1 = peritos.some((el) => el.email === user.email);
-
-//         //  console.log('userPerito1', userPerito1);
-//         //  console.log('Perito1', peritos);
-//         if (userPerito1) {
-//           setEmailUser(user.email);
-//           //GUARDAR EN EL LOCALSTORAGE USER.EMAIL
-//           navigate("/");
-//         } else {
-//           alert("comuniquese con su administrador");
-//         }
-//         // ...
-//       });
-//     } catch (error) {
-//       return alert("comuniquese con su administrador");
-//     }
-//   };
-  
-  
+  }
+ 
 
 
+  const submitHandlerGoogle = (e) => {
+    /* console.log('e.target', e.target) */
+    let booll = peritos.some((el) => el.email === "email");
 
-  const handleSignin = () => {
-    auth.signInWithPopup(provider).catch(alert);
+    try {
+      
+        signInWithPopup(auth, googleProvider).then((result) => {
+          // This gives you a Google Access Token. You can use it to access the Google API.
+          // const credential = GoogleAuthProvider.credentialFromResult(result);
+          // console.log(credential)
+          // const token = credential.accessToken;
+          // The signed-in user info.
+          const user = result.user;
+         console.log('user', user) 
+         let userPerito1=peritos.some((el) => el.email === user.email)
+         
+        //  console.log('userPerito1', userPerito1);
+        //  console.log('Perito1', peritos);
+          if(userPerito1){
+            setEmailUser(user.email)
+            //GUARDAR EN EL LOCALSTORAGE USER.EMAIL
+            navigate("/user");
+          }
+          else{
+            alert("comuniquese con su administrador")
+          }
+          // ...
+        })
+    } catch (error) {
+      return alert("comuniquese con su administrador")
+    }
+    
   };
+
+  
   return (
-    <C.Container>
-      <C.Button onClick={handleSignin}>Login com Google</C.Button>
-    </C.Container>
+    <div className="body">
+      <div className="contenedor">
+        <div className="screen">
+          <div className="screen__content">
+          
+            <div className="social-login">
+           
+              <div className="social-icons">
+                <Button
+                variant="primary"
+                  type="submit"
+                  onClick={(e) => submitHandlerGoogle(e)}
+                  className="social-login__icon fab fa-instagram"
+                  style={{fontSize: "12px", backgroundColor:"blue"}}
+                >
+                  Google
+                </Button>
+                <a href="#" className="social-login__icon fab fa-facebook"></a>
+                <a href="#" className="social-login__icon fab fa-twitter"></a>
+              </div>
+            </div>
+          </div>
+          <div className="screen__background">
+            <span className="screen__background__shape screen__background__shape4"></span>
+            <span className="screen__background__shape screen__background__shape3"></span>
+            <span className="screen__background__shape screen__background__shape2"></span>
+            <span className="screen__background__shape screen__background__shape1"></span>
+          </div>
+        </div>
+      </div>
+    </div>
   );
-
-
-//   return (
-//     <div className="social-icons">
-//       <Button
-//         variant="primary"
-//         type="submit"
-//         onClick={(e) => submitHandlerGoogle(e)}
-//         className="social-login__icon fab fa-instagram"
-//         style={{ fontSize: "12px", backgroundColor: "blue" }}
-//       >
-//         Iniciar Sesion
-//       </Button>
-//     </div>
-//   );
-// }
+}
 
 export default Login;
 
