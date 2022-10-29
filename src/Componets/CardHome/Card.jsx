267a9ed635c7510e792f08e64ba9694a1c9008ab
Neleon10 <cvn45@hotmail.com>
2022-10-28 23:27:09 -0300

    adding new container to App

--- src/Componets/CardHome/Card.jsx
+++ src/Componets/CardHome/Card.jsx
@@ -3,48 +3,48 @@ import { useDispatch, useSelector } from "react-redux";
 import car from '../../assets/car.gif'
 import './CardHome.css'
 
-function Card({consult,clearConsult}) {
-  const[loader, setLoader]= useState(false)
+function Card({ consult, clearConsult }) {
+  const [loader, setLoader] = useState(false)
   const dispatch = useDispatch()
 
   useEffect(() => {
     setLoader(true)
     setTimeout(() => {
-        setLoader(false)
+      setLoader(false)
     }, 2000)
-}, [])
+  }, [])
 
 
 
-    return (
+  return (
     <>
-        {
-          loader ? (
-            <img src={car} alt="Cargando" width="350px" />
-          ): 
-          <div className='cardHomeContainer'> 
-          <div className="parrafosContainer">
+      {
+        loader ? (
+          <img src={car} alt="Cargando" width="350px" />
+        ) :
+          <div className='cardHomeContainer'>
+            <div className="parrafosContainer">
               <p className='parrafosCardHome'><span>Asegurado:</span> <strong>{consult[0].Nombre}</strong>
               </p>
 
-              <button className='cardCloseButton'   onClick={()=> clearConsult()}>X</button>
-              
+              <button className='cardCloseButton' onClick={() => clearConsult()}>X</button>
+
               <p className='parrafosCardHome'><span>Patente:</span>  {consult[0].Patente}</p>
               <p className='parrafosCardHome'><span>Telefono:</span>  {consult[0].celular}</p>
               <p className='parrafosCardHome'><span>Aseguradora:</span>  {consult[0].Compañia}</p>
 
               <p className='parrafosCardHome'><span>Estado del caso: </span>
-                {!consult[0].estado && "Sin Completar" }
+                {!consult[0].estado && "Sin Completar"}
                 {consult[0].estado === 'Pericia finalizada' ? <span className='ended'>{consult[0].estado}</span> : consult[0].estado}
               </p>
-              
-              <p className='parrafosCardHome'><span>El Perito encargado es: </span>{consult[0].perito}</p>
-              <p className='parrafosCardHome'><span>Notas Extras:</span>  {!consult[0].notas ? "Sin Completar" : consult[0].notas}</p>  
-          </div>
-          </div>
-        }
 
-      </>
+              <p className='parrafosCardHome'><span>El Perito encargado es: </span>{consult[0].perito}</p>
+              <p className='parrafosCardHome'><span>Notas Extras:</span>  {!consult[0].notas ? "Sin Completar" : consult[0].notas}</p>
+            </div>
+          </div>
+      }
+
+    </>
   )
 }
 
