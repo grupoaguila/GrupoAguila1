201ba6d5cb2e3a8754daf6a3fc978a8e52a8b96a
Neleon10 <cvn45@hotmail.com>
2022-11-02 14:00:57 -0300

    adding more styles to text

--- src/Componets/Services/Services.jsx
+++ src/Componets/Services/Services.jsx
@@ -18,18 +18,16 @@ function Services() {
             Servicios
           </span>
           <span>Ofrecidos</span>
-          <spane style = {{color:"black"}}>
-          Nuestro trabajo es brindar servicios de peritajes en nuestro centro de inspección, 
-            <br />
-            capacitación, investigación de métodos de reparación de daños materiales, 
-            <br />
-            liquidaciones de siniestros, auditorías externas e internas de daños materiales, 
-            <br />
-            desarrollos de tecnologías aplicadas a peritaciones en modo real, convenio de 
-            <br />
-            precios con talleres homologados y repuesteras locales. 
-          </spane>
-          
+          <div className="serviceText">
+            <p>
+              Nuestro trabajo es brindar servicios de peritajes en nuestro centro de inspección,
+              capacitación, investigación de métodos de reparación de daños materiales,
+              liquidaciones de siniestros, auditorías externas e internas de daños materiales,
+              desarrollos de tecnologías aplicadas a peritaciones en modo real, convenio de
+              precios con talleres homologados y repuesteras locales.
+            </p>
+          </div>
+
           <div
             className="blur s-blur1"
             style={{ background: "blue" }}
@@ -39,7 +37,7 @@ function Services() {
         {/* right side */}
       </div>
       <div className="w-right">
-      <motion.div
+        <motion.div
           initial={{ rotate: 45 }}
           whileInView={{ rotate: 0 }}
           viewport={{ margin: "-40px" }}
@@ -48,25 +46,25 @@ function Services() {
         >
           <div className="w-secCircle">
             <span>INSPECCIONES</span>
-           {/*  <img src={Upwork} alt="" /> */}
+            {/*  <img src={Upwork} alt="" /> */}
           </div>
           <div className="w-secCircle">
-          <span>INVESTIGACIONES</span>
-       {/*      <img src={Fiverr} alt="" /> */}
+            <span>INVESTIGACIONES</span>
+            {/*      <img src={Fiverr} alt="" /> */}
           </div>
           <div className="w-secCircle">
-          <span>CAPACITACIONES</span>
+            <span>CAPACITACIONES</span>
             {/* <img src={Amazon} alt="" /> */}
           </div>{" "}
           <div className="w-secCircle">
-          <span>CONSULTORÍA</span>
-          {/*   <img src={Shopify} alt="" /> */}
+            <span>CONSULTORÍA</span>
+            {/*   <img src={Shopify} alt="" /> */}
           </div>
           <div className="w-secCircle">
-          <span>POSICIONAMIENTO</span>
+            <span>POSICIONAMIENTO</span>
             {/* <img src={Facebook} alt="" /> */}
           </div>
-          </motion.div>
+        </motion.div>
         {/* background Circles */}
         <div className="w-backCircle blueCircle"></div>
         <div className="w-backCircle yellowCircle"></div>
