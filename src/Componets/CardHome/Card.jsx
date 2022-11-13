694a90449a1a0093f783af63173f35f6e997e0a9
gabdala17 <gisella.abdala@gmail.com>
2022-11-12 23:19:34 -0300

    no encontrado

--- src/Componets/CardHome/Card.jsx
+++ src/Componets/CardHome/Card.jsx
@@ -7,7 +7,10 @@ function Card({ consult, clearConsult }) {
   const [loader, setLoader] = useState(false)
   const dispatch = useDispatch()
   const peritos=useSelector(states=>states.peritos)
-  const infoPerito= peritos.filter(e=>e.nombre===consult[0].perito)
+  let infoPerito
+  if(consult[0].hasOwnProperty('perito')){
+   infoPerito=  peritos.filter(e=>e.nombre===consult[0].perito)
+  }
   console.log(infoPerito)
 
   useEffect(() => {
@@ -22,30 +25,41 @@ function Card({ consult, clearConsult }) {
   return (
     <>
       {
-        loader ? (
+        loader ? 
           <img src={car} alt="Cargando" width="350px" />
-        ) :
-          <div className='cardHomeContainer'>
-            <div className="parrafosContainer">
-              <p className='parrafosCardHome'><span>Asegurado:</span> <strong>{consult[0].Nombre}</strong>
-              </p>
+        :
+        <div>
 
-              <button className='cardCloseButton' onClick={() => clearConsult()}>X</button>
-
-              <p className='parrafosCardHome'><span>Patente:</span>  {consult[0].Patente}</p>
-              <p className='parrafosCardHome'><span>Aseguradora:</span>  {consult[0].Compañia}</p>
-
-              <p className='parrafosCardHome'><span>Estado del caso: </span>
-                {!consult[0].estado && "Sin Completar"}
-                {consult[0].estado === 'Pericia finalizada' ? <span className='ended'>{consult[0].estado}</span> : consult[0].estado}
-              </p>
-
-              <p className='parrafosCardHome'><span>Perito encargado: </span>{consult[0].perito}</p>
-              <p className='parrafosCardHome'><span>Telefono:</span>  {infoPerito[0]?.celular}</p>
-              <p className='parrafosCardHome'><span>Notas Extras:</span>  {!consult[0].notas ? "Sin Completar" : consult[0].notas}</p>
-              <p className='parrafosCardHome'><span>Repuestos/Taller</span> "Sin Completar" </p>
+        {
+          consult[0] === 'No se encontró su caso' ?
+            (
+              <p> su caso no ha sido cargado </p>
+            ):
+            <div className='cardHomeContainer'>
+              <div className="parrafosContainer">
+                <p className='parrafosCardHome'><span>Asegurado:</span> <strong>{consult[0].Nombre}</strong>
+                </p>
+    
+                <button className='cardCloseButton' onClick={() => clearConsult()}>X</button>
+    
+                <p className='parrafosCardHome'><span>Patente:</span>  {consult[0].Patente}</p>
+                <p className='parrafosCardHome'><span>Aseguradora:</span>  {consult[0].Compañia}</p>
+    
+                <p className='parrafosCardHome'><span>Estado del caso: </span>
+                  {!consult[0].estado && "Sin Completar"}
+                  {consult[0].estado === 'Pericia finalizada' ? <span className='ended'>{consult[0].estado}</span> : consult[0].estado}
+                </p>
+    
+                <p className='parrafosCardHome'><span>Perito encargado: </span>{consult[0].perito}</p>
+                <p className='parrafosCardHome'><span>Telefono:</span>  {infoPerito[0]?.celular}</p>
+                <p className='parrafosCardHome'><span>Notas Extras:</span>  {!consult[0].notas ? "Sin Completar" : consult[0].notas}</p>
+                <p className='parrafosCardHome'><span>Repuestos/Taller</span> "Sin Completar" </p>
+              </div>
             </div>
-          </div>
+
+        }
+      </div>
+        
       }
 
     </>
--- src/Componets/SearchBar/SearchBar.jsx
+++ src/Componets/SearchBar/SearchBar.jsx
@@ -17,22 +17,31 @@ function SearchBar() {
     setPatents(e.target.value);
   };
   
-  console.log('patents1', patents)
   const handleSubmit = async (e) => {
     e.preventDefault(); 
     let patents1= patents.toUpperCase().trim()
+    console.log('patents1', patents1)
     const doc = await getCasesByCondition(patents1);
-    setConsult(doc);
+    console.log('docbuscado=>',doc);
+    if(doc.length === 0){
+      console.log('doc.length=>',doc.length);
+      setConsult(['No se encontró su caso']);
+      return setPatents("")
+    }
+    
+    setConsult(doc)
     setPatents("");
   };
   
-  console.log('doc', consult)
+  console.log('consult', consult)
+  console.log('cargando', patents)
   return (
     <>
       <form action="" className="search-bar" onSubmit={handleSubmit}>
         <input
           type="search"
           name="search"
+          // value={patents}
           pattern=".*\S.*"
           placeholder="Nro. Patente"
           onChange={(e) => handleChange(e)}
@@ -42,11 +51,17 @@ function SearchBar() {
           <span>Search</span>
         </button>
       </form>
-      {consult.length > 0 && (
+      {consult.length > 0 &&  (
         
           <Card consult={consult} clearConsult={clearConsult} />
         
-      )}
+        )
+      }
+      {/* {
+        consult.length > 0 && consult[0]=== 'No se encontró su caso' &&(
+          <p> su caso no ha sido cargado </p>
+        )
+      } */}
     </>
   );
 }
